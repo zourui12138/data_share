c2f896922b029064b5dd99a9000822ed4ce7134c
zourui <zourui@mrray.cn>
2018-04-17 14:05:38 +0800

    moment---时间处理

--- src/main.js
+++ src/main.js
@@ -16,8 +16,7 @@ import $ from 'jquery'
 import moment from 'moment/moment'
 Vue.filter('moment', function (value, formatString) {
     formatString = formatString || 'YYYY-MM-DD HH:mm:ss';
-    // return moment(value).format(formatString); // value可以是普通日期 20170723
-    return moment.unix(value).format(formatString); // 这是时间戳转时间
+    return moment(parseInt(value)).format(formatString); // value可以是普通日期 20170723
 });
 
 Vue.directive('scroll', {
